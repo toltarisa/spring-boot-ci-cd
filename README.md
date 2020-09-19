12b1e34782d334587aedac51f727fa02691666ff
Isa Toltar <isatoltar@gmail.com>
2020-09-19 16:46:56 +0300

    spring starter project created

---
+++ README.md
@@ -0,0 +1,1 @@
+CI/CD Example with TravisCI
---
+++ src/main/java/com/example/springbootcicd/SpringBootCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCiCdApplication.class, args);
+	}
+
+}
