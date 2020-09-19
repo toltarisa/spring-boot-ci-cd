0ef89f3d05c614865ade13d8f4793b716bf9c553
Isa Toltar <isatoltar@gmail.com>
2020-09-19 20:32:04 +0300

    example test file added

---
+++ src/test/java/com/example/springbootcicd/SpringBootCiCdApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.springbootcicd;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringBootCiCdApplicationTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
